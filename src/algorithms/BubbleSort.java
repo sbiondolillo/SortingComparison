172a981623eaf182bd5138b870a0e8a83f720680
Samuel Biondolillo <biwinning121@gmail.com>
2017-11-17 13:44:49 -0500

    build BubbleSort and include it in main()

---
+++ src/algorithms/BubbleSort.java
@@ -0,0 +1,43 @@
+/*
+ * BubbleSort Class
+ * Samuel Biondolillo
+ * CIS210M:ZZ Data Structures and Elementary Algorithms
+ * Goal: To implement the BubbleSort Algorithm for sorting integers
+ * Version  0.0.1   11/17/17
+ */
+
+package algorithms;
+
+public class BubbleSort {
+	
+	public static int[] sort(int[] sourceArray) {
+		
+		
+		int[] sourceArrayCopy = sourceArray.clone();
+		int sourceArrayCopyLength = sourceArrayCopy.length;
+		
+		int temp;
+		for (int i = 0; i < sourceArrayCopyLength; i++) {
+			
+			boolean swapped = false;
+			for (int j = 0; j < sourceArrayCopyLength - i - 1; j++) {
+				
+				if (sourceArrayCopy[j] > sourceArrayCopy[j+1]) {
+					
+					temp = sourceArrayCopy[j];
+					sourceArrayCopy[j] = sourceArrayCopy[j+1];
+					sourceArrayCopy[j+1] = temp;
+					swapped = true;
+					
+				}
+			}
+			
+			if (!swapped)
+				break;
+			
+		}
+		return sourceArray;
+		
+	}
+
+}
--- src/main/SortingComparison.java
+++ src/main/SortingComparison.java
@@ -8,32 +8,35 @@
 
 package main;
 
+import algorithms.BubbleSort;
+
 public class SortingComparison {
 
 	public static void main(String[] args) {
 		
 		RandomNumberGenerator rng = new RandomNumberGenerator(100000);
 		int[] randomNumbers = rng.getNumbers();
+		int[] results;
 		
 		final long startTime1 = System.currentTimeMillis();
-		// call .sort() on first class
-		final long duration1 = (System.currentTimeMillis() - startTime1) / 1000;
+		results = BubbleSort.sort(randomNumbers);
+		final double duration1 = (System.currentTimeMillis() - startTime1) / 1000.0;
 		
 		final long startTime2 = System.currentTimeMillis();
 		// call .sort on second class
-		final long duration2 = (System.currentTimeMillis() - startTime2) / 1000;
+		final double duration2 = (System.currentTimeMillis() - startTime2) / 1000.0;
 		
 		final long startTime3 = System.currentTimeMillis();
 		// call .sort on third class
-		final long duration3 = (System.currentTimeMillis() - startTime3) / 1000;
+		final double duration3 = (System.currentTimeMillis() - startTime3) / 1000.0;
 		
 		System.out.println("Three different algorithms were run to sort a randomly-generated list of 1000 integers.");
 		System.out.println();
 		System.out.println("The results were as follows:");
 		System.out.println();
-		System.out.println("Algorithm 1: " + duration1 + "seconds");
-		System.out.println("Algorithm 2: " + duration2 + "seconds");
-		System.out.println("Algorithm 3: " + duration3 + "seconds");
+		System.out.printf("Bubble Sort: aproximately %.03f seconds%n", duration1);
+		System.out.println("Algorithm 2: " + duration2 + " seconds");
+		System.out.println("Algorithm 3: " + duration3 + " seconds");
 		
 
 	}
